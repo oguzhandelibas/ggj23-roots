c7bb09cd91a36a679369f18fb3b442f677a854c8
boracun <cun2424@gmail.com>
2023-02-04 01:50:09 +0300

    +add: Randomly spawn enemies

--- Assets/1_Scripts/AI/EnemyController.cs
+++ Assets/1_Scripts/AI/EnemyController.cs
@@ -15,5 +15,9 @@ public class EnemyController : MonoBehaviour
     protected virtual void Update()
     {
         transform.Translate(Time.deltaTime * EnemyData.Speed * moveDirection);
+        if (Vector3.Distance(Vector3.zero, transform.position) <= 0.5f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
