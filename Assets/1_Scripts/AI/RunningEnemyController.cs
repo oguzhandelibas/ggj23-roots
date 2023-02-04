d34977704edabbb1cd569d741df116cb6de530a5
boracun <cun2424@gmail.com>
2023-02-04 15:55:29 +0300

    +add: integrate running enemy assets

--- Assets/1_Scripts/AI/RunningEnemyController.cs
+++ Assets/1_Scripts/AI/RunningEnemyController.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class RunningEnemyController : EnemyController
 {
-    
+    private Animator runningEnemyAnimator;
+
+    protected override void Start()
+    {
+        base.Start();
+        runningEnemyAnimator = GetComponent<Animator>();
+        runningEnemyAnimator.speed = 2f;
+    }
 }
