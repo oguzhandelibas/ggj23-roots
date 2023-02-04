39299dcb39df8a591552ada49f2fa6ab77fdcf7e
boracun <cun2424@gmail.com>
2023-02-04 15:30:51 +0300

    +add: hit effects for walking enemy

--- Assets/1_Scripts/AI/WalkingEnemyController.cs
+++ Assets/1_Scripts/AI/WalkingEnemyController.cs
@@ -1,6 +1,14 @@
+using System;
 using UnityEngine;
 
 public class WalkingEnemyController : EnemyController
 {
-    
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hit");
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
 }
